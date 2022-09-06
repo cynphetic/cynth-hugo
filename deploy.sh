6f241e421bb1f8ca24d4668944d1e5568cbfac16
nyhryan <nyhryan99102@gmail.com>
2022-09-06 22:31:14 +0900

    rebuilding site Tue Sep  6 22:31:14     2022

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo -t hugo-theme-geppaku
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
 fi
+git commit -m "$msg"
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
+# Push source and build repos.
+git push origin master
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+# Come Back up to the Project Root
+cd ..
 
-echo "Removing existing files"
-rm -rf public/*
 
-echo "Generating site"
-git submodule update --init --recursive --depth=1
-hugo --minify
+# blog 저장소 Commit & Push
+git add .
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
 
-git push --all
+git push origin master
